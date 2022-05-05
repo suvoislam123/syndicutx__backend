e27678fd1754cc2d6afd610be51f2c432a10b609
suvoislam123 <suvoislam753@gmail.com>
2022-05-05 14:57:59 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
 const app = express();
-const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zfajy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('SyndicutX_Shop').collection('products');
+
+        //Authentication API
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESSTOKEN_SECRET, {
+                expiresIn:'30d'
+            });
+            res.send(accessToken);
+        })
+
+        // Services API
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+    } finally {
+        
+    }
+    
+
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('The warehouse server is running');
 });
