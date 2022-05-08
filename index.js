c5f3d67311c569c44e2151a875145877c0aa7a2a
suvoislam123 <suvoislam753@gmail.com>
2022-05-08 21:11:20 +0600

    server jwt token authentication fully added

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { response } = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zfajy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -38,6 +55,34 @@ async function run() {
             res.send(data);
 
         });
+        app.post('/updateproduct/:id', async (req, res) => {
+            const updateproduct = req.body;
+            console.log(updateproduct)
+            console.log(updateproduct);
+            const product_id = req.params.id;
+            const query = { _id: ObjectId(product_id) };
+            const product = await productsCollection.findOne(query);
+            console.log(product);
+            //jodi product null hoi tar mane product ta deleted
+            const result = await productsCollection.updateOne(
+                { _id: ObjectId(product_id) },
+                {
+                    $set:
+                    {
+                        name: updateproduct.name,
+                        image: updateproduct.image,
+                        price: updateproduct.price,
+                        quantity: updateproduct.quantity,
+                        supplier_name: updateproduct.supplier_name,
+                        short_description: updateproduct.short_description,
+                        catagory: updateproduct.catagory
+
+                    }
+
+                }
+            );
+            res.send(result);
+        })
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -67,11 +112,58 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const order = await ordersCollection.findOne(query);
             res.send(order);
-        },[])
+        }, [])
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = ordersCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
         //API for message
         app.post('/message/:email', async (req, res) => {
             //I will do it later
-        })
+        });
+        //API for update Quantity
+        app.post('/updatequantity', async (req, res) => {
+            const orderedQuantity = req.body.quantity;
+            const product_id = req.body.product_id;
+            const query = { _id: ObjectId(product_id) };
+            const product = await productsCollection.findOne(query);
+            console.log(product);
+            //jodi product null hoi tar mane product ta deleted
+            const result = await productsCollection.updateOne(
+                { _id: ObjectId(product_id) },
+                {
+                    $set:
+                    {
+                        quantity: product.quantity - orderedQuantity
+                    }
+
+                }
+            );
+            res.send(result);
+        });
+
+        //Messages API 
+        app.post('/messages', async (req, res) => {
+            const message = req.body;
+            const result = await messagesCollection.insertOne(message);
+            res.send(result);
+
+        });
+        app.delete('/orders/:id', async (req, res) => {
+            const order_id = req.params.id;
+            const query = { _id: ObjectId(order_id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result);
+        });
 
     } finally {
         
